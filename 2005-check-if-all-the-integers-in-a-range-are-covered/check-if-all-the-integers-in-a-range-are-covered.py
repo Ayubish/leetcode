cb82a103826edfadfef823896158226e89f15782
Ayub Nasir <119772377+Ayubish@users.noreply.github.com>
2025-02-03 11:04:16 +0300

    Time: 3 ms (18.01%) | Memory: 17.6 MB (75.88%) - LeetSync

--- 2005-check-if-all-the-integers-in-a-range-are-covered/check-if-all-the-integers-in-a-range-are-covered.py
+++ 2005-check-if-all-the-integers-in-a-range-are-covered/check-if-all-the-integers-in-a-range-are-covered.py
@@ -3,5 +3,7 @@ class Solution:
         full_set = set()
         for arr in ranges:
             full_set = full_set.union({x for x in range(arr[0], arr[1]+1)})
-        sub_set = {x for x in range(left, right+1)}
-        return sub_set.issubset(full_set)
+        for i in range(left, right+1):
+            if i not in full_set:
+                return False
+        return True

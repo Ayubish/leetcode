32abf54f89548d53362f9cc2adf5ae8c9e668ba3
Ayub Nasir <119772377+Ayubish@users.noreply.github.com>
2025-02-06 11:13:07 +0300

    Time: 3 ms (56.93%) | Memory: 18 MB (8.40%) - LeetSync

---
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findTheWinner(self, n: int, k: int) -> int:
+        nums = [x for x in range(1,n+1)]
+        i = 0
+        while len(nums)>1:
+            i = (i+k-1)%len(nums)
+            nums.pop(i)
+        return nums[0]
+
+        

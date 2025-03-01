62cd696c51ad2cb64b5bf8a147026030c971e271
Ayub Nasir <119772377+Ayubish@users.noreply.github.com>
2025-03-01 15:02:23 +0300

    Time: 423 ms (37.79%) | Memory: 46.6 MB (16.85%) - LeetSync

---
+++ 1364-tuple-with-same-product/tuple-with-same-product.py
@@ -0,0 +1,12 @@
+class Solution:
+    def tupleSameProduct(self, nums: List[int]) -> int:
+        dicto = defaultdict(int)
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                dicto[nums[i]*nums[j]] += 1
+        result = 0
+        print(dicto)
+        for val in dicto.values():
+            if val >= 2:
+                result += (val*(val-1))//2
+        return result*8

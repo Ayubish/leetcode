64ce8d106d89654f91aca546046afdde3adbe393
Ayub Nasir <119772377+Ayubish@users.noreply.github.com>
2025-04-03 17:57:47 +0300

    Time: 53 ms (10.52%) | Memory: 26.3 MB (99.72%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.py
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.py
@@ -1,5 +1,17 @@
 class Solution:
     def findDuplicates(self, nums: List[int]) -> List[int]:
-        dicto = Counter(nums)
-        ans = [item for item in dicto if dicto[item] == 2]
+        ans = []
+        i = 0
+        while i<len(nums):
+            pos = nums[i]-1
+            if nums[i] == -1:
+                i += 1
+            elif pos != i:
+                if nums[i] == nums[pos]:
+                    ans.append(nums[i])
+                    nums[i], nums[pos] = -1, -1
+                else:
+                    nums[pos], nums[i] = nums[i], nums[pos]
+            else:
+                i += 1
         return ans
